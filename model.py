d1b1b51b4af8efaf142d6c36c33ff1c392a9efbf
tungduong123q@gmail.com <tungduong123q@gmail.com>
2024-10-12 13:40:34 +0700

    model

---
+++ model.py
@@ -0,0 +1,252 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+import torch.nn.functional as F
+import torch.optim as optim
+from torch import nn
+from utils import *
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
+
+class BackboneResnet50(nn.Module):
+    def __init__(self):
+        super().__init__()
+        model = torchvision.models.resnet50(weights='IMAGENET1K_V1', progress=True)
+        feature_maps = list(model.children())[:8]
+        self.backbone = nn.Sequential(*feature_maps)
+        for param in self.backbone.named_parameters():
+            param[1].requires_grad = True
+        
+    def forward(self, img_data):
+        return self.backbone(img_data)
+
+class ProposalModule(nn.Module):
+    def __init__(self, in_features, hidden_dim=512, n_anchors=9, dropout=0.3):
+        super().__init__()
+        self.n_anchors = n_anchors
+        self.conv1 = nn.Conv2d(in_channels=in_features, out_channels=hidden_dim, kernel_size=3, padding=1)
+        self.dropout = nn.Dropout(dropout)
+        self.conv_head = nn.Conv2d(in_channels=hidden_dim, out_channels=n_anchors, kernel_size=1)
+        self.reg_head = nn.Conv2d(in_channels=hidden_dim, out_channels=n_anchors*4, kernel_size=1)
+        
+    def forward(self, feature_map, pos_anc_ind=None, neg_anc_ind=None, pos_anc_coords=None):
+        # set mode
+        if pos_anc_ind is None or neg_anc_ind is None or pos_anc_coords is None:
+            mode = 'eval'
+        else:
+            mode = 'train'
+            
+        # Input: feature map -> output: vector 512d
+        output = self.conv1(feature_map)
+        output = F.relu(self.dropout(output))
+        
+        conf_score_pred = self.conv_head(output)    # anchors has object -> (B, n_anchor, h, w)
+        reg_offsets_pred = self.reg_head(output)    # the offset of bbox -> (B, 4*n_anchor, h, w)
+        
+        if mode == 'train':
+            # get conf score for each anchor
+            conf_score_pos = conf_score_pred.flatten()[pos_anc_ind]
+            conf_score_neg = conf_score_pred.flatten()[neg_anc_ind]
+            # get offset for each anchor
+            offsets_pos = reg_offsets_pred.contiguous().view(-1,4)[pos_anc_ind]
+            # proposal of offset
+            proposals = generate_proposals(pos_anc_coords, offsets_pos)
+            
+            return conf_score_pos, conf_score_neg, offsets_pos, proposals
+
+        elif mode == 'eval':
+            return conf_score_pred, reg_offsets_pred
+    
+class RegionProposalNetwork(nn.Module):
+    def __init__(self, img_size, out_size, out_channels):
+        super().__init__()
+        
+        self.img_height, self.img_width = img_size
+        self.out_h, self.out_w = out_size
+        
+        # downscale factor
+        self.width_scale_factor = self.img_height // self.out_h
+        self.height_scale_factor = self.img_width // self.out_w
+        
+        # scales and ratios of each anchor box
+        self.anc_scales = [2, 4, 6]
+        self.anc_ratios = [0.5, 1, 1.5]
+        self.n_anc_boxes = 9
+        
+        # IoU thresholds
+        self.pos_thres = 0.7
+        self.neg_thres = 0.3
+        
+        # Weight loss
+        self.w_conf = 1
+        self.w_reg = 5
+        
+        self.feature_extractor = BackboneResnet50()
+        self.proposal_module = ProposalModule(in_features=out_channels)
+        
+    def forward(self, images, gt_bboxes, gt_classes):
+        batch_size = images.size(dim=0)
+        feature_map = self.feature_extractor(images)
+        
+        # generate anchors
+        anc_x, anc_y = gen_anc_centers(out_size=(self.out_h, self.out_w))
+        anc_base = gen_anc_base(anc_x, anc_y, self.anc_scales, self.anc_ratios, (self.out_h, self.out_w))
+        anc_bboxes_all = anc_base.repeat(batch_size, 1,1,1,1)
+        
+        anc_bboxes_all.to(device)
+        
+        # get positive and negative anchors
+        gt_bboxes_proj = project_bboxes(gt_bboxes, self.width_scale_factor, self.height_scale_factor, mode='p2f')
+        positive_anc_ind, negative_anc_ind, GT_conf_scores, GT_offsets, GT_class_pos, positive_anc_coords, negative_anc_coords, positive_anc_ind_sep = get_req_anchors(anc_bboxes_all, gt_bboxes_proj, gt_classes)
+        conf_scores_pos, conf_scores_neg, offsets_pos, proposals = self.proposal_module(feature_map, positive_anc_ind, \
+                                                                                        negative_anc_ind, positive_anc_coords)
+        
+        cls_loss = calc_cls_loss(conf_scores_pos, conf_scores_neg, batch_size)
+        reg_loss = calc_bbox_reg_loss(GT_offsets, offsets_pos, batch_size)
+        
+        total_rpn_loss = self.w_conf * cls_loss + self.w_reg * reg_loss
+        
+        return total_rpn_loss, feature_map, proposals, positive_anc_ind_sep, GT_class_pos
+    
+    def inference(self, images, conf_thresh=0.5, nms_thresh=0.7):
+        with torch.no_grad():
+            batch_size = images.size(dim=0)
+            feature_map = self.feature_extractor(images)
+
+            # generate anchors
+            anc_pts_x, anc_pts_y = gen_anc_centers(out_size=(self.out_h, self.out_w))
+            anc_base = gen_anc_base(anc_pts_x, anc_pts_y, self.anc_scales, self.anc_ratios, (self.out_h, self.out_w))
+            anc_boxes_all = anc_base.repeat(batch_size, 1, 1, 1, 1)
+            anc_boxes_flat = anc_boxes_all.reshape(batch_size, -1, 4)
+
+            # get conf scores and offsets
+            conf_scores_pred, offsets_pred = self.proposal_module(feature_map)
+            conf_scores_pred = conf_scores_pred.reshape(batch_size, -1)
+            offsets_pred = offsets_pred.reshape(batch_size, -1, 4)
+
+            # filter out proposals based on conf threshold and nms threshold for each image
+            proposals_final = []
+            conf_scores_final = []
+            for i in range(batch_size):
+                conf_scores = torch.sigmoid(conf_scores_pred[i])
+                offsets = offsets_pred[i]
+                anc_boxes = anc_boxes_flat[i]
+                proposals = generate_proposals(anc_boxes, offsets)
+                # filter based on confidence threshold
+                conf_idx = torch.where(conf_scores >= conf_thresh)[0]
+                conf_scores_pos = conf_scores[conf_idx]
+                proposals_pos = proposals[conf_idx]
+                # filter based on nms threshold
+                nms_idx = ops.nms(proposals_pos, conf_scores_pos, nms_thresh)
+                conf_scores_pos = conf_scores_pos[nms_idx]
+                proposals_pos = proposals_pos[nms_idx]
+
+                proposals_final.append(proposals_pos)
+                conf_scores_final.append(conf_scores_pos)
+            
+        return proposals_final, conf_scores_final, feature_map
+    
+def calc_cls_loss(conf_scores_pos, conf_scores_neg, batch_size):
+    target_pos = torch.ones_like(conf_scores_pos)
+    target_neg = torch.zeros_like(conf_scores_neg)
+    
+    target = torch.cat((target_pos, target_neg))
+    inputs = torch.cat((conf_scores_pos, conf_scores_neg))
+    
+    loss = F.binary_cross_entropy_with_logits(inputs, target, reduction='sum') * 1. / batch_size
+    
+    return loss
+
+def calc_bbox_reg_loss(gt_offsets, reg_offsets_pos, batch_size):
+    assert gt_offsets.size() == reg_offsets_pos.size()
+    loss = F.smooth_l1_loss(reg_offsets_pos, gt_offsets, reduction='sum') * 1. / batch_size
+    return loss
+
+
+class ClassificationModule(nn.Module):
+    def __init__(self, out_channels, n_classes, roi_size, hidden_dim=512, p_dropout=0.3):
+        super().__init__()        
+        self.roi_size = roi_size
+        # hidden network
+        self.avg_pool = nn.AvgPool2d(self.roi_size)
+        self.fc = nn.Linear(out_channels, hidden_dim)
+        self.dropout = nn.Dropout(p_dropout)
+        
+        # define classification head
+        self.cls_head = nn.Linear(hidden_dim, n_classes)
+        
+    def forward(self, feature_map, proposals_list, gt_classes=None):
+        
+        if gt_classes is None:
+            mode = 'eval'
+        else:
+            mode = 'train'
+        
+        # apply roi pooling on proposals followed by avg pooling
+        roi_out = ops.roi_pool(feature_map, proposals_list, self.roi_size)
+        roi_out = self.avg_pool(roi_out)
+        
+        # flatten the output
+        roi_out = roi_out.squeeze(-1).squeeze(-1)
+        
+        # pass the output through the hidden network
+        out = self.fc(roi_out)
+        out = F.relu(self.dropout(out))
+        
+        # get the classification scores
+        cls_scores = self.cls_head(out)
+        
+        if mode == 'eval':
+            return cls_scores
+        
+        # compute cross entropy loss
+        cls_loss = F.cross_entropy(cls_scores, gt_classes.long())
+        
+        return cls_loss
+
+
+class TwoStageDetector(nn.Module):
+    def __init__(self, img_size, out_size, out_channels, n_classes, roi_size):
+        super().__init__() 
+        self.rpn = RegionProposalNetwork(img_size, out_size, out_channels)
+        self.classifier = ClassificationModule(out_channels, n_classes, roi_size)
+        
+    def forward(self, images, gt_bboxes, gt_classes):
+        total_rpn_loss, feature_map, proposals, \
+        positive_anc_ind_sep, GT_class_pos = self.rpn(images, gt_bboxes, gt_classes)
+        
+        # get separate proposals for each sample
+        pos_proposals_list = []
+        batch_size = images.size(dim=0)
+        for idx in range(batch_size):
+            proposal_idxs = torch.where(positive_anc_ind_sep == idx)[0]
+            proposals_sep = proposals[proposal_idxs].detach().clone()
+            pos_proposals_list.append(proposals_sep)
+        
+        cls_loss = self.classifier(feature_map, pos_proposals_list, GT_class_pos)
+        total_loss = cls_loss + total_rpn_loss
+        
+        return total_loss
+    
+    def inference(self, images, conf_thresh=0.5, nms_thresh=0.7):
+        batch_size = images.size(dim=0)
+        proposals_final, conf_scores_final, feature_map = self.rpn.inference(images, conf_thresh, nms_thresh)
+        cls_scores = self.classifier(feature_map, proposals_final)
+        
+        # convert scores into probability
+        cls_probs = F.softmax(cls_scores, dim=-1)
+        print(cls_probs)
+        # get classes with highest probability
+        classes_all = torch.argmax(cls_probs, dim=-1)
+        
+        classes_final = []
+        # slice classes to map to their corresponding image
+        c = 0
+        for i in range(batch_size):
+            n_proposals = len(proposals_final[i]) # get the number of proposals for each image
+            classes_final.append(classes_all[c: c+n_proposals])
+            c += n_proposals
+            
+        return proposals_final, conf_scores_final, classes_final
